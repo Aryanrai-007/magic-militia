5858d7a7e62909647e3d1dbabe7e596b2f14bf48
Aryan Rai <raiaryaneng@gmail.com>
2026-08-13 14:34:58 +0530

    feat: open champion picker on site entry

--- web/character-fix.js
+++ web/character-fix.js
@@ -4,3 +4,4 @@ applyCharacterCardSprites();
 new MutationObserver(applyCharacterCardSprites).observe(document.getElementById('characterGrid'),{childList:true});
 const characterDraw=drawWizard;
 drawWizard=(ctx,p,glow,me=false)=>{if(p===local&&selectedCharacter){ctx.save();ctx.translate(p.x,p.y);ctx.imageSmoothingEnabled=false;ctx.shadowBlur=me?22:12;ctx.shadowColor=glow;if(MAGIC_SHEET.complete)ctx.drawImage(MAGIC_SHEET,selectedCharacter.sx*4,selectedCharacter.sy*4,64,64,-32,-56,64,64);ctx.restore();return}characterDraw(ctx,p,glow,me)};
+setTimeout(()=>{document.getElementById('characterPicker').classList.add('open');renderChars();},250);
